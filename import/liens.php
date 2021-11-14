2b16b589cbefcaad870ea12b77bae76065814377
cahitYes <efeyesiloglu@hotmail.com>
2021-11-14 21:01:28 +0100

    portfolio 2eme echeance

--- import/liens.php
+++ import/liens.php
@@ -19,15 +19,25 @@
     <?php
     include "menu.php";
     ?>
-    <h1>Voici quelque liens utiles</h1>
-    <h2>w3c validator</h2>
-    <a href="https://validator.w3.org/">ici</a>
-    <h2>MDN</h2>
-    <a href="https://developer.mozilla.org/fr/">ici</a>
-    <h2>php.net</h2>
-    <a href="https://www.php.net/">ici</a>
-    <h2>Github</h2>
-    <a href="https://github.com/">ici</a>
+   <div id="conteneurliens">
+        <h1>Voici quelque liens utiles</h1>
+           <div id="liens"> 
+               <h2>w3c validator</h2>
+                    <img src="img/w3c.png" alt=""/><a href="https://validator.w3.org/" target="_blank">ici</a>
+            </div>
+            <div id="liens">
+                <h2>MDN</h2>
+                <img src="img/mdn.png" alt=""/><a href="https://developer.mozilla.org/fr/" target="_blank">ici</a>
+            </div>
+            <div id="liens"> 
+                <h2>php.net</h2>
+                <img src="img/php.png" alt=""/> <a href="https://www.php.net/" target="_blank">ici</a>
+            </div>
+            <div id="liens">
+                    <h2>Github</h2>
+                    <img src="img/git.png" alt=""/><a href="https://github.com/" target="_blank">ici</a>
+            </div>
+   </div>
 </body>
 
 </html>
--- import/tutoriel.php
+++ import/tutoriel.php
@@ -13,10 +13,37 @@
 </head>
 <body>
     
-    <h1>tutoriel</h1>
+    
     <?php
     include "menu.php";
     ?>
-
+    <div id="tutoriel">
+    <p>Pour ma section tutoriel j'ai choisis les fonctions en PHP.</p>
+    <h3>Mais c'est quoi une fonction?</h3> 
+    <p>Une fonction est une série d'instructions qui effectue des actions et qui retourne une valeur.
+    En général, dès que vous avez besoin d'effectuer des opérations un peu longues dont vous aurez à nouveau besoin plus tard,
+    il est conseillé de vérifier s'il n'existe pas déjà une fonction qui fait cela pour vous. 
+    Et si la fonction n'existe pas, vous avez la possibilité de la créer.</p>   
+    <p>
+    à la suite de mes recherches je vais vous parlez de deux tutoriel différent. Celui de OpenClassRoom et celui de "comment ca marche"
+    Suite a une brève comparaison et pour des raisons totalement personnel ( car oui chaqu'un est libre d'aimer se qu'il  veut ) je vais vous dirigé vers celui de OpenClassRoom, mais vous retrouverai également
+    le liens vers l autre tutoriel</p>
+        <h3>Pourquoi celui de OpenClassRoom?</h3>
+    <p>
+        C'est tres simple, je trouve que les tutoriels d'OpenClassRoom sont vraiment accessible a tout le monde, que l'on soit débutant ou agueri,
+         les cours / tuto / conseils sont vraiment à la porté de tous !! 
+        J'aime la touche d'humour qu'ils mettent dans leur examples, et surtout tout est expliqué pas par pas.
+        Ils prennent le temps de répondre a des questions assez basique et prennent le temps d'expliqué tout depuis les bases.
+     </p>
+     <h3>Ou trouvé se super Tutoriel?</h3>
+    <p> Ne vous inquiété pas !! Je vous met le liens vers la parti des fonctions de OpenClassRoom <a href="https://openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql/912352-exploitez-toute-la-puissance-des-fonctions-php" target="_blank">ici</a>
+</p>
+<h3>Mon deuxieme choix</h3>
+<p>Le deuxieme tutoriel que j'ai choisis est celui de commentcamarche.net, je ne vais pas dire que ce tutoriel n'est pas bien car toute les infos donné sont utiles et il y a également des exemples, cependant,
+    je trouve que ce tutoriel est beaucoup moin complet, moins détaillé meme si on reponds au grande question.
+    Je trouve que ce tutoriel pourrait convenir a des gens un peu plus agueri et qui ont deja des connaissance en php.
+    Vous trouverez le liens vers celui ci <a href="https://web.maths.unsw.edu.au/~lafaye/CCM/php/phpfonc.htm" target="_blank">ici </a>
+</p>
+    </div>
 </body>
 </html>
